d34b59bd1c6a7e52398f4685d5193095e6768cfc
Prasad Saya <38682661+prsaya@users.noreply.github.com>
2023-08-28 12:09:53 +0530

    Update dbFunctions.js
    Code formatting

--- dbFunctions.js
+++ dbFunctions.js
@@ -3,7 +3,7 @@ const getItems = function(conn, callback) {
 	const sql = 'SELECT * FROM items';
 
 	conn.query(sql, function (error, results) {
-	    callback(error, results);
+		callback(error, results);
 	});
 }
 
@@ -11,18 +11,18 @@ const addItem = function(conn, data, callback) {
 
 	const sql = 'INSERT INTO items SET ?';
 
-    conn.query(sql, data, function (error, results) {
-	    callback(error, results);
-    });
+	conn.query(sql, data, function (error, results) {
+		callback(error, results);
+	});
 }
 
 const deleteItems = function(conn, name, callback) {
 
 	const sql = 'DELETE FROM items WHERE name = ?';
 
-    conn.query(sql, [ name ], function (error, results) {
-	    callback(error, results);
-    });
+	conn.query(sql, [ name ], function (error, results) {
+		callback(error, results);
+	});
 }
 
 const updateItems = function(conn, data, callback) {
@@ -31,7 +31,7 @@ const updateItems = function(conn, data, callback) {
 	const sql = 'UPDATE items SET quantity = ? WHERE name = ?';
 
 	conn.query(sql, updateData, function(error, results) {
-	    callback(error, results);
+		callback(error, results);
 	});
 }
 
